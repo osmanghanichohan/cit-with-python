ea6aebb84fefcc18fd0d4f60df090925376f29a2
osmanghanichohan <osmanghaniengg@gmail.com>
2025-05-05 16:08:46 +0500

    Add files via upload
    py

---
+++ Question..3.py
@@ -0,0 +1,18 @@
+ #Write Python code to calculate the area of the following shapes
+#base=5
+#height=6
+#side=4.5
+#side=4.5
+#π=3.14...
+#radius=3
+#lenght=7
+#widht=5
+
+Area=0.5*5*6
+Area=4.5*4.5
+Area=3.14*6
+Area=7*5
+print(0.5*5*6)
+print(4.5*4.5)
+print(3.14*6)
+print(7*5)
